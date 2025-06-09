358265aa63646f3f3adb13b8c7c98eb5bdfb974e
xcheche <chekwubechukwu23@gmail.com>
2025-06-09 07:06:30 +0100

    test_register_user/test_register_user_duplicate_email
    authentication testing

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,14 +1,21 @@
 from django.urls import reverse
 import pytest
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+
+
 from django.test.client import Client
 from django.contrib.auth.hashers import check_password
-
+from django.contrib.messages import get_messages
 from accounts.models import PendingUser
+from django.contrib.messages.storage.base import Message
+
+
+User = get_user_model()
     
+pytestmark = pytest.mark.django_db
 
-
-def test_register_user(db, client: Client):
+def test_register_user(client: Client):
     url = reverse('register')
     data = {
         'password': 'testuser',
@@ -20,10 +27,32 @@ def test_register_user(db, client: Client):
     
     assert pending_user
     assert check_password(data['password'], pending_user.password)
+    messages = list(get_messages(response.wsgi_request))
+    assert len(messages) == 1
+    assert messages[0].tags == 'success'
+    # Assuming the success message is set in the view
+    assert "verification code sent to" in str(messages[0]).lower()
 
 
-def test_register_user_duplicate_email(client):
-    pass
+
+def test_register_user_duplicate_email(client: Client, user_instance):
+    url = reverse('register')
+    data = {
+        'password': 'testuser',
+        'email': user_instance.email  # already exists
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 302 # Redirect to register page
+    # Check if the user is redirected to the register page
+    assert response.url == reverse('register')
+
+    messages = list(get_messages(response.wsgi_request))
+    assert len(messages) == 1
+    assert messages[0].tags == 'error'
+    assert "email exists" in messages[0].message.lower()
+
+
 
 
 def test_verify_account_valid_code():
--- conftest.py
+++ conftest.py
@@ -1,6 +1,23 @@
 from django.test.client import Client
 import pytest
 
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+
 @pytest.fixture
 def client():
     return Client()
+
+
+# pytest fixture to create a user instance for testing
+
+@pytest.fixture
+def user_instance(db):
+    return User.objects.create_user(
+        email='testuser@example.com',
+        password='testpassword123'
+    )
